a94426d769728d6e4a45e2529d8a34f57a97f347
Canson <helanzheng@qq.com>
2018-07-20 10:02:26 +0800

    no message

--- Format.js
+++ Format.js
@@ -14,3 +14,24 @@ export const Money=s=>{
   s=s.replace(/,(\d\d)$/,".$1");  
   return s.replace(/^\./,"0.");
 }
+
+// 科学计数法转数字
+export const Enum=r=>{
+  let str = r.toString().split('e-');
+  if (str.length < 2) return r.toString();
+  r = '0.';
+  for (let i = 1; i < Number(str[1]); i++) r += '0';
+  return r + str[0].replace('.', '');
+}
+
+// 比特币金额
+export const BitAmount=n=>{
+  if(n==''||isNaN(n)) return '0';
+  n=Enum(n).split('.');
+  if(n.length>1){
+    if (n[1].length > 8) n[1]=n[1].substring(0,8);
+    return n[0]+'.'+n[1];
+  }else{
+    return n[0]
+  }
+}
